0081d9503fb7441b7a184850cba06961b462b95b
LocKey <rudfhr97@naver.com>
2024-08-05 21:45:16 +0900

    Task 3. @ControllerAdvice / 애플리케이션 전역 예외 처리

--- spring-mvc-3/initial/src/main/java/cholog/GlobalExceptionHandler.java
+++ spring-mvc-3/initial/src/main/java/cholog/GlobalExceptionHandler.java
@@ -1,2 +1,16 @@
-package cholog;public class GlobalExceptionHandler {
+package cholog;
+
+import cholog.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
 }
