7fe57909298c0a3232a1272dd1f6174e2ce62c30
Haon <88240193+msung99@users.noreply.github.com>
2024-08-06 10:40:43 +0900

    feat: search 에 대한 exceptionhandler 구현

--- spring-mvc-3/initial/src/main/java/cholog/controller/ProductController.java
+++ spring-mvc-3/initial/src/main/java/cholog/controller/ProductController.java
@@ -1,18 +1,22 @@
 package cholog.controller;
 
 import cholog.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<String> searchException(IllegalArgumentException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 
     @GetMapping("/products")
     public ResponseEntity<Void> searchProduct(@RequestParam String keyword) {
         if (true) {
             throw new IllegalArgumentException("Invalid keyword: " + keyword);
         }
-
         return ResponseEntity.ok().build();
     }
 
