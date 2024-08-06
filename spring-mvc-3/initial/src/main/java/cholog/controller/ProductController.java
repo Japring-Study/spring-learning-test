ef5055dcce3399387c02727391cc7aa444fb49a1
gib.son <shai4956@gmail.com>
2024-08-06 23:03:01 +0900

    [spring-mvc-3] 2. `@ExceptionHandler` 적용

--- spring-mvc-3/initial/src/main/java/cholog/controller/ProductController.java
+++ spring-mvc-3/initial/src/main/java/cholog/controller/ProductController.java
@@ -1,27 +1,36 @@
 package cholog.controller;
 
-import cholog.exception.NotFoundException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import cholog.exception.NotFoundException;
 
 @RestController
 public class ProductController {
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<Void> handleIllegalArgumentException() {
+		return ResponseEntity.badRequest().build();
+	}
 
-    @GetMapping("/products")
-    public ResponseEntity<Void> searchProduct(@RequestParam String keyword) {
-        if (true) {
-            throw new IllegalArgumentException("Invalid keyword: " + keyword);
-        }
+	@GetMapping("/products")
+	public ResponseEntity<Void> searchProduct(@RequestParam String keyword) {
+		if (true) {
+			throw new IllegalArgumentException("Invalid keyword: " + keyword);
+		}
 
-        return ResponseEntity.ok().build();
-    }
+		return ResponseEntity.ok().build();
+	}
 
-    @GetMapping("/products/{id}")
-    public ResponseEntity<Void> getProduct(@PathVariable Long id) {
-        if (true) {
-            throw new NotFoundException("Product not found: id=" + id);
-        }
+	@GetMapping("/products/{id}")
+	public ResponseEntity<Void> getProduct(@PathVariable Long id) {
+		if (true) {
+			throw new NotFoundException("Product not found: id=" + id);
+		}
 
-        return ResponseEntity.ok().build();
-    }
+		return ResponseEntity.ok().build();
+	}
 }
