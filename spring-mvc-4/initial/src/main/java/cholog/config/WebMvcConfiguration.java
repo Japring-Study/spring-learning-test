406d289e5824fd7e5481ae271d3f9ab96005dcf1
gib.son <shai4956@gmail.com>
2024-08-07 01:02:37 +0900

    3. admin이 아닌 경우 /admin/** 엔드포인트에 접근하지 못하도록 인터셉터 추가

--- spring-mvc-4/initial/src/main/java/cholog/config/WebMvcConfiguration.java
+++ spring-mvc-4/initial/src/main/java/cholog/config/WebMvcConfiguration.java
@@ -8,17 +8,19 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import cholog.ui.CheckLoginInterceptor;
+
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("hello");
 	}
-
-	// TODO: "/admin/**" 요청 시 LoginInterceptor 동작하게 하기
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-
+		registry.addInterceptor(new CheckLoginInterceptor())
+			.addPathPatterns("/admin/**");
 	}
 
 	// TODO: AuthenticationPrincipalArgumentResolver 등록하기
