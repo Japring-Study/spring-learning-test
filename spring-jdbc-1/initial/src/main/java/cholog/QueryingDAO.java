dd09eca4fc67b3b29d642dd34e418c036e04f6db
gib.son <shai4956@gmail.com>
2024-08-11 05:26:16 +0900

    feat: `query`와 `rowMapper`를 사용해 where 조건에 맞는 객체 리스트 조회

--- spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
+++ spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
@@ -65,7 +65,8 @@ public class QueryingDAO {
 	 */
 	public List<Customer> findCustomerByFirstName(String firstName) {
 		String sql = "select id, first_name, last_name from customers where first_name = ?";
-		//TODO : firstName을 기준으로 customer를 list형태로 반환
-		return null;
+		List<Customer> matchedCustomers = jdbcTemplate.query(sql, actorRowMapper, firstName);
+
+		return matchedCustomers;
 	}
 }
