48d1251829acd477dae1ae70f2576b01a740ee42
LocKey <rudfhr97@naver.com>
2024-08-12 23:26:32 +0900

    Task 3. Repository / Repository를 통해 CRUD 작업을 수행하는 메서드를 제공

--- spring-data-jpa-1/initial/src/main/java/cholog/CustomerRepository.java
+++ spring-data-jpa-1/initial/src/main/java/cholog/CustomerRepository.java
@@ -1,12 +1,12 @@
 package cholog;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
     List<Customer> findByLastName(String lastName);
     List<Customer> findByLastNameIgnoreCase(String lastName);
     List<Customer> findByLastNameOrderByFirstNameDesc(String lastName);
