966073c219e2b45968d7b372a98affc0a7bc3cad
LocKey <rudfhr97@naver.com>
2024-08-12 23:24:16 +0900

    Task 2. EntityMapping / @Entity, @Id 어노테이션을 활용하여 객체를 테이블과 매핑

--- spring-data-jpa-1/initial/src/main/java/cholog/CustomerRepository.java
+++ spring-data-jpa-1/initial/src/main/java/cholog/CustomerRepository.java
@@ -1,5 +1,13 @@
 package cholog;
 
-public interface CustomerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    List<Customer> findByLastName(String lastName);
+    List<Customer> findByLastNameIgnoreCase(String lastName);
+    List<Customer> findByLastNameOrderByFirstNameDesc(String lastName);
 }
