0bea06b7712cbf1e4b312c6c5ce4a1ac6c226c49
DESKTOP-J6GSER3\user <dayae.dev@gmail.com>
2024-08-12 22:09:42 +0900

    3.3 Object with RowMapper, 주어진 Id에 해당하는 customer를 객체로 반환

--- spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
+++ spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
@@ -47,8 +47,19 @@ public class QueryingDAO {
      */
     public Customer findCustomerById(Long id) {
         String sql = "select id, first_name, last_name from customers where id = ?";
-        //TODO : 주어진 Id에 해당하는 customer를 객체로 반환
-        return null;
+
+        Customer customer = jdbcTemplate.queryForObject(
+                sql,
+                (resultSet, rowNum) -> {
+                    Customer customer1 = new Customer(
+                            resultSet.getLong("id"),
+                            resultSet.getString("fist_name"),
+                            resultSet.getString("last_name")
+                    );
+                    return customer1;
+                }, id);
+
+        return customer;
     }
 
     /**
