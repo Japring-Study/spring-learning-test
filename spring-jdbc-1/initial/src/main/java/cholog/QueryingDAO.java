4d1f848620868064d8fdc8034432339d0d374730
msung99 <msung6924@naver.com>
2024-08-13 10:02:26 +0900

    feat: getLastName 기능 구현

--- spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
+++ spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
@@ -39,7 +39,7 @@ public class QueryingDAO {
      */
     public String getLastName(Long id) {
         //TODO : 주어진 Id에 해당하는 customers의 lastName을 반환
-        return null;
+        return jdbcTemplate.queryForObject("select last_name from customers where id = ?", String.class, id);
     }
 
     /**
