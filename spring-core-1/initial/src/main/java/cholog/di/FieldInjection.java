e1c91a809e8141fb6f95a544e3765efc502d2307
DESKTOP-J6GSER3\user <dayae.dev@gmail.com>
2024-08-14 11:22:03 +0900

    Feat: 2.3. Field Injection, 주석 삭제

--- spring-core-1/initial/src/main/java/cholog/di/FieldInjection.java
+++ spring-core-1/initial/src/main/java/cholog/di/FieldInjection.java
@@ -6,9 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class FieldInjection {
 
-    /*
-    FieldInjection으로 InjectionBean 주입받기
-     */
     @Autowired
     private InjectionBean injectionBean;
 
