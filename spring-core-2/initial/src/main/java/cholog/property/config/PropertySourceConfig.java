8a6865b4afadbdb200782fa2c6a7e6b0778f0742
LocKey <rudfhr97@naver.com>
2024-08-18 22:50:57 +0900

    Task 2.2. Using @PropertySource and @Value / Value annotation을 통해 프로퍼티 값 가져오기

--- spring-core-2/initial/src/main/java/cholog/property/config/PropertySourceConfig.java
+++ spring-core-2/initial/src/main/java/cholog/property/config/PropertySourceConfig.java
@@ -3,6 +3,7 @@ package cholog.property.config;
 import cholog.property.GoogleDriveRestClient;
 import cholog.property.GoogleMapsRestClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -25,10 +26,8 @@ public class PropertySourceConfig {
         return new GoogleMapsRestClient(env.getProperty("google.api.endpoint"));
     }
 
-    // TODO: ext-api.properties의 google.api.endpoint 값을 어노테이션을 사용해서 가져오기
-    // TODO: 위 endpoint 값을 사용하여 GoogleMapsRestClient를 빈으로 등록하기
     @Bean
-    public GoogleDriveRestClient googleDriveRestClient() {
-        return new GoogleDriveRestClient("");
+    public GoogleDriveRestClient googleDriveRestClient(@Value("${google.api.endpoint}") String endpoint) {
+        return new GoogleDriveRestClient(endpoint);
     }
 }
