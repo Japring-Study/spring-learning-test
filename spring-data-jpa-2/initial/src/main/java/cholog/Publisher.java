7ccf9532b13ebf12610dfd836756326e51cf8d98
yohan <dygks924@gmail.com>
2024-08-18 16:31:39 +0900

    feat: 3 다대일 양방향 해결

--- spring-data-jpa-2/initial/src/main/java/cholog/Publisher.java
+++ spring-data-jpa-2/initial/src/main/java/cholog/Publisher.java
@@ -1,10 +1,9 @@
 package cholog;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -14,8 +13,12 @@ public class Publisher {
     private Long id;
     private String name;
 
+    @OneToMany(mappedBy = "publisher")
+    private Set<Book> books;
+
     public Publisher(String name) {
         this.name = name;
+        this.books = new HashSet<>();
     }
 
     public Publisher() {
@@ -30,10 +33,10 @@ public class Publisher {
     }
 
     public void addBook(Book book) {
-
+        books.add(book);
     }
 
     public Set<Book> getBooks() {
-        return null;
+        return books;
     }
 }
