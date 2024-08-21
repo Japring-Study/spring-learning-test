415d136b42c1dcefac0e08ed5b2586c1adfad72f
gib.son <shai4956@gmail.com>
2024-08-21 19:17:30 +0900

    feat: 구현 완료

--- spring-core-2/initial/src/main/java/cholog/property/config/AuthConfig.java
+++ spring-core-2/initial/src/main/java/cholog/property/config/AuthConfig.java
@@ -1,11 +1,16 @@
 package cholog.property.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
 import cholog.property.JwtTokenKeyProvider;
 
-// TODO: Java-based Configuration을 하기 위한 클래스로 지정하기
+@Configuration
 public class AuthConfig {
-    // TODO: application.properties의 security.jwt.token.secret-key 값을 활용하여 JwtTokenKeyProvider를 빈으로 등록하기
-    public JwtTokenKeyProvider jwtTokenKeyProvider() {
-        return new JwtTokenKeyProvider("");
-    }
+	@Value("${security.jwt.token.secret-key")
+	String secretKey;
+
+	public JwtTokenKeyProvider jwtTokenKeyProvider() {
+		return new JwtTokenKeyProvider(secretKey);
+	}
 }
--- spring-data-jpa-1/initial/src/main/java/cholog/Customer.java
+++ spring-data-jpa-1/initial/src/main/java/cholog/Customer.java
@@ -1,38 +1,45 @@
 package cholog;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String lastName;
 
-    private Long id;
-    private String firstName;
-    private String lastName;
+	public Customer() {
+	}
 
-    public Customer() {
-    }
+	public Customer(Long id, String firstName, String lastName) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
 
-    public Customer(Long id, String firstName, String lastName) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+	public Customer(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
 
-    public Customer(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void updateFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public void updateFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 }
