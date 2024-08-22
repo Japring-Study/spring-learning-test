731ab69ca474be713e9e2947454dc1cb596de490
yohan <dygks924@gmail.com>
2024-08-22 16:52:52 +0900

    feat: 3-2 RestClient Get 요청의 응답 변환하기 해결

--- spring-http-client-1/initial/src/main/java/cholog/TodoClientWithRestClient.java
+++ spring-http-client-1/initial/src/main/java/cholog/TodoClientWithRestClient.java
@@ -15,8 +15,6 @@ public class TodoClientWithRestClient {
     }
 
     public List<Todo> getTodos() {
-        // TODO: restClient의 get 메서드를 사용하여 요청을 보내고 결과를 Todo 리스트로 변환하여 반환
-
         return restClient.get()
                 .uri("/todos")
                 .retrieve()
@@ -24,8 +22,9 @@ public class TodoClientWithRestClient {
     }
 
     public Todo getTodoById(Long id) {
-        // TODO: restClient의 get 메서드를 사용하여 요청을 보내고 결과를 Todo로 변환하여 반환
-        // TODO: 존재하지 않는 id로 요청을 보낼 경우 TodoException.NotFound 예외를 던짐
-        return new Todo();
+        return restClient.get()
+                .uri("/todos/{todoId}", id)
+                .retrieve()
+                .body(Todo.class);
     }
 }
