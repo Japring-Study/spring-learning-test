07a1c835b0826f4f0c815105ae46fb84bf80ebb8
yohan <dygks924@gmail.com>
2024-08-22 16:58:01 +0900

    feat: 3-3 RestClient 예외 처리 해결

--- spring-http-client-1/initial/src/main/java/cholog/TodoClientWithRestClient.java
+++ spring-http-client-1/initial/src/main/java/cholog/TodoClientWithRestClient.java
@@ -2,6 +2,7 @@ package cholog;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.RestClient;
 
 import java.util.Collections;
@@ -25,6 +26,9 @@ public class TodoClientWithRestClient {
         return restClient.get()
                 .uri("/todos/{todoId}", id)
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+                    throw new TodoException.NotFound(id);
+                })
                 .body(Todo.class);
     }
 }
