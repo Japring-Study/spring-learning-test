b5f6f91425735dd4d6aec484fda69557f4960e57
LocKey <rudfhr97@naver.com>
2024-08-26 18:51:09 +0900

    Task 4. RestTemplate - post, put, delete 구현

--- spring-http-client-1/initial/src/main/java/cholog/Todo.java
+++ spring-http-client-1/initial/src/main/java/cholog/Todo.java
@@ -1,11 +1,24 @@
 package cholog;
 
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+
+//@Data               // getter, setter
+//@NoArgsConstructor  // 기본 생성자 생성
+//@AllArgsConstructor   // 모든 필드를 포함하는 생성자 생성
 public class Todo {
 
     private Long userId;
     private Long id;
     private String title;
     private boolean completed;
+
+    public Todo(Long userId, Long id, String title, boolean completed) { // -> AllArgsConstructor (Lombok)
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+    }
 
     public Long getUserId() { return userId; }
 
--- spring-http-client-1/initial/src/main/java/cholog/TodoClientWithRestTemplate.java
+++ spring-http-client-1/initial/src/main/java/cholog/TodoClientWithRestTemplate.java
@@ -1,7 +1,6 @@
 package cholog;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,6 +22,47 @@ public class TodoClientWithRestTemplate {
                 throw new TodoException.NotFound(id);
             } else {
                 throw new TodoException("다른 에러 : " + e.getMessage());
+            }
+        }
+    }
+
+    public Todo createTodo(Todo newTodo) {
+        String resourceUrl = "http://jsonplaceholder.typicode.com/todos";
+        try {
+            ResponseEntity<Todo> response = restTemplate.postForEntity(resourceUrl, newTodo, Todo.class);
+            return response.getBody();
+        } catch (HttpClientErrorException e) {
+            throw new TodoException("Todo 생성 중 에러 발생: " + e.getMessage());
+        }
+    }
+
+    public Todo updateTodo(Long id, Todo updatedTodo) {
+        String resourceUrl = "http://jsonplaceholder.typicode.com/todos/{id}";
+        try {
+            HttpEntity<Todo> requestUpdate = new HttpEntity<>(updatedTodo);
+            ResponseEntity<Todo> response = restTemplate.exchange(resourceUrl, HttpMethod.PUT, requestUpdate, Todo.class, id);
+            return response.getBody();
+        } catch (HttpClientErrorException e) {
+            if(e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                throw new TodoException.NotFound(id);
+            } else {
+                throw new TodoException("Todo 업데이트 중 에러 발생: " + e.getMessage());
+            }
+        }
+    }
+
+    public HttpStatusCode deleteTodo(Long id) {
+        try {
+            String resourceUrl = "http://jsonplaceholder.typicode.com/todos/{id}";
+            restTemplate.delete(resourceUrl, id);
+//            ResponseEntity<Void> response = restTemplate.exchange(resourceUrl, HttpMethod.DELETE, null, Void.class, id);
+//            return response.getStatusCode();
+            return HttpStatus.OK;
+        } catch (HttpClientErrorException e) {
+            if(e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                throw new TodoException.NotFound(id);
+            } else {
+                throw new TodoException("Todo 삭제 중 에러 발생: " + e.getMessage());
             }
         }
     }
