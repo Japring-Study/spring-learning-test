ba25eff4ac7714489ee23956c0d80cbf004de920
LocKey <rudfhr97@naver.com>
2024-08-26 19:38:37 +0900

    Task 4. RestClient - post, put, delete 구현

--- spring-http-client-1/initial/src/main/java/cholog/TodoClientWithRestClient.java
+++ spring-http-client-1/initial/src/main/java/cholog/TodoClientWithRestClient.java
@@ -34,4 +34,29 @@ public class TodoClientWithRestClient {
                 .body(Todo.class);
         return todo;
     }
+
+    public Todo createTodo(Todo newTodo) {
+        Todo createdTodo = restClient.post()
+                .uri("/todos")
+                .body(newTodo)
+                .retrieve()
+                .body(Todo.class);
+        return createdTodo;
+    }
+
+    public Todo updateTodo(Long id, Todo updatedTodo) {
+        Todo updatedResult = restClient.put()
+                .uri("/todos/{id}", id)
+                .body(updatedTodo)
+                .retrieve()
+                .body(Todo.class);
+        return updatedResult;
+    }
+
+    public void deleteTodo(Long id) {
+        restClient.delete()
+                .uri("/todos/{id}", id)
+                .retrieve()
+                .toBodilessEntity(); // DELETE 요청에 대한 응답 본문이 필요 없으므로 toBodilessEntity 사용
+    }
 }
