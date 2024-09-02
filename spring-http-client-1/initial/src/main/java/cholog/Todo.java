1a44ec75fc434d87a4072a9d33ff0acb93c11727
dayaelee <dayae.dev@gmail.com>
2024-09-02 16:51:15 +0900

    Feat:2. RestTemplate

--- spring-http-client-1/initial/src/main/java/cholog/Todo.java
+++ spring-http-client-1/initial/src/main/java/cholog/Todo.java
@@ -3,6 +3,10 @@ package cholog;
 public class Todo {
 
     // TODO: Todo 객체가 가지는 필드들을 정의
+    private String userId;
+    private String id;
+    private String title;
+    private Boolean completed;
 
     public String getTitle() {
         return null;
