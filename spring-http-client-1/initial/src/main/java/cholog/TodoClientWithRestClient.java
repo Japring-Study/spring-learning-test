6dd80676263d4ab8964e2f7c693bb3fcbf542b4c
dayaelee <dayae.dev@gmail.com>
2024-09-02 17:29:14 +0900

    Feat:3-2. GET 요청의 응답 변환하기

--- spring-http-client-1/initial/src/main/java/cholog/TodoClientWithRestClient.java
+++ spring-http-client-1/initial/src/main/java/cholog/TodoClientWithRestClient.java
@@ -23,8 +23,12 @@ public class TodoClientWithRestClient {
     }
 
     public Todo getTodoById(Long id) {
-        // TODO: restClient의 get 메서드를 사용하여 요청을 보내고 결과를 Todo로 변환하여 반환
-        // TODO: 존재하지 않는 id로 요청을 보낼 경우 TodoException.NotFound 예외를 던짐
-        return new Todo();
+
+        Todo result = restClient.get()
+                .uri("http://jsonplaceholder.typicode.com/todos/{id}", id)
+                .retrieve()
+                .body(Todo.class);
+
+        return result;
     }
 }
